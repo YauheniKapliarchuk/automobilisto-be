dbe25024213146abb0e78caa9b627e3cf3a6cd2b
yauhenikapliarchuk <yauheni.kapliarchuk@gmail.com>
2022-12-28 00:03:47 +0300

    00000-Add_default_project.

---
+++ src/main/java/com/automobilisto_de/AutomobilistDeApplication.java
@@ -0,0 +1,13 @@
+package com.automobilisto_de;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobilistDeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobilistDeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automobilisto_de/automobilistde/AutomobilistDeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automobilisto_de.automobilistde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobilistDeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
